ca82517ba5659ab16fda053ae78191401120a5bc
Dacuna97 <diegoacunaberger@gmail.com>
2021-07-12 23:07:09 +0100

    new components

---
+++ components/events/EventsSearch.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function EventsSearch({}) {
+  return (
+    <form>
+      <div>
+        <div>
+          <label htmlFor='year'>Year</label>
+          <select id='year'></select>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+export default EventsSearch;
